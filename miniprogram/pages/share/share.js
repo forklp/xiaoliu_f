5f2fc7bb8f153b22d165a937115fff3c5cc2583d
MilesTin <1154520245@qq.com>
2019-02-16 13:58:23 +0800

    add icons and add incomplete share ui, waiting for projects plan

---
+++ miniprogram/pages/share/share.js
@@ -0,0 +1,106 @@
+// pages/share/share.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    countries:[
+      {
+      countryName:"美国",
+      //countryShortName决定了寻找的图标
+      countryShortName:"usa"},
+      {
+        countryName: "英国",
+        //countryShortName决定了寻找的图标
+        countryShortName: "uk"
+      },
+      {
+        countryName: "欧洲",
+        //countryShortName决定了寻找的图标
+        countryShortName: "europe"
+      },
+      {
+        countryName: "新加坡",
+        //countryShortName决定了寻找的图标
+        countryShortName: "singapore"
+      },
+      {
+        countryName: "日本",
+        //countryShortName决定了寻找的图标
+        countryShortName: "japan"
+      },
+      {
+        countryName: "香港",
+        //countryShortName决定了寻找的图标
+        countryShortName: "hongKong"
+      },
+      {
+        countryName: "澳新",
+        //countryShortName决定了寻找的图标
+        countryShortName: "australia"
+      },
+      {
+        countryName: "加拿大",
+        //countryShortName决定了寻找的图标
+        countryShortName: "canada"
+      },
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
